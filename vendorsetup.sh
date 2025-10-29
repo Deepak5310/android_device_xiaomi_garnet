e5f8377e06ab569dc616d38f1c03a85eddb92c9c
Deepak5310 <37282143+Deepak5310@users.noreply.github.com>
2025-10-29 15:01:06 +0530

    garnet: vendorsetup.sh: Refactor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,16 +1,16 @@
 # Remove
-rm -rf out vendor/xiaomi/garnet kernel/xiaomi/sm7435 kernel/xiaomi/sm7435-modules kernel/xiaomi/sm7435-devicetrees device/xiaomi/garnet-miuicamera vendor/xiaomi/garnet-miuicamera
+rm -rf out
 
 # Device Source
-git clone -b lineage-23.0 --depth 1 https://github.com/garnet-random/proprietary_vendor_xiaomi_garnet vendor/xiaomi/garnet
+rm -rf vendor/xiaomi/garnet && git clone -b lineage-23.0 --depth 1 https://github.com/garnet-random/proprietary_vendor_xiaomi_garnet vendor/xiaomi/garnet
 
-git clone -b lineage-23.0 --depth 1 https://github.com/garnet-random/android_kernel_xiaomi_sm7435 kernel/xiaomi/sm7435
-git clone -b lineage-23.0 --depth 1 https://github.com/garnet-random/android_kernel_xiaomi_sm7435-modules kernel/xiaomi/sm7435-modules
-git clone -b lineage-23.0 --depth 1 https://github.com/garnet-random/android_kernel_xiaomi_sm7435-devicetrees kernel/xiaomi/sm7435-devicetrees
+rm -rf kernel/xiaomi/sm7435 && git clone -b lineage-23.0 --depth 1 https://github.com/Lafactorial/android_kernel_xiaomi_sm7435 kernel/xiaomi/sm7435 # KernelSU
+rm -rf kernel/xiaomi/sm7435-modules && git clone -b lineage-23.0 --depth 1 https://github.com/garnet-random/android_kernel_xiaomi_sm7435-modules kernel/xiaomi/sm7435-modules
+rm -rf kernel/xiaomi/sm7435-devicetrees && git clone -b lineage-23.0 --depth 1 https://github.com/garnet-random/android_kernel_xiaomi_sm7435-devicetrees kernel/xiaomi/sm7435-devicetrees
 
 # MIUI Camera
-git clone -b lineage-23.0 --depth 1 https://github.com/scas-projects/device_xiaomi_garnet-miuicamera device/xiaomi/garnet-miuicamera
-git clone -b lineage-23.0 --depth 1 https://gitlab.com/c0pt4n/proprietary_vendor_xiaomi_garnet-miuicamera vendor/xiaomi/garnet-miuicamera
+rm -rf device/xiaomi/garnet-miuicamera && git clone -b lineage-23.0 --depth 1 https://github.com/scas-projects/device_xiaomi_garnet-miuicamera device/xiaomi/garnet-miuicamera
+rm -rf vendor/xiaomi/garnet-miuicamera && git clone -b lineage-23.0 --depth 1 https://gitlab.com/c0pt4n/proprietary_vendor_xiaomi_garnet-miuicamera vendor/xiaomi/garnet-miuicamera
 
 # Hardware Xiaomi
 [ ! -d ./hardware/xiaomi ] && git clone -b lineage-23.0 --depth 1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
