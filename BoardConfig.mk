kleidione/device_xiaomi_garnet

garnet: Include MiuiCamera if exists
Change-Id: Iee675b4924cb2be7ac2d36c7737b06edc9cadfc9

BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/xiaomi/garnet
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
+# Inherit from proprietary files for miuicameraAdd commentMore actionsAdd commentMore actions
+-include device/xiaomi/miuicamera-garnet/BoardConfig.mk
+
 # A/B
 AB_OTA_PARTITIONS := \
     boot \

Deepak5310/android_device_xiaomi_garnet

Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/xiaomi/garnet
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
+# Inherit from proprietary files for miuicamera
+-include device/xiaomi/garnet-miuicamera/BoardConfig.mk
+
 # A/B
 AB_OTA_PARTITIONS := \
     boot \
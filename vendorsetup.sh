1e9a1b5b479a3fd4331c27234bc87a8cff0f02e0
AdarshGrewal <adarshgrewal@gmail.com>
2025-07-02 12:46:03 -0300

    garnet: Include MiuiCamera if exists
    Change-Id: Iee675b4924cb2be7ac2d36c7737b06edc9cadfc9
    Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+git clone -b lineage-23.0 --depth 1 https://github.com/scas-projects/device_xiaomi_garnet-miuicamera device/xiaomi/garnet-miuicamera
+git clone -b lineage-23.0 --depth 1 https://gitlab.com/c0pt4n/proprietary_vendor_xiaomi_garnet-miuicamera vendor/xiaomi/garnet-miuicamera
